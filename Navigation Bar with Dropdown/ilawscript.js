92b7dea277666ca3ddce87b7c7dc6beb09c7652f
Jhermaine Bautista <202412274@gordoncollege.edu.ph>
2025-04-07 17:06:28 +0800

    Add files via upload

---
+++ Navigation Bar with Dropdown/ilawscript.js
@@ -0,0 +1,7 @@
+    function turnOnLight() {
+        document.getElementById('myImage').src = 'pic_bulbon.gif';
+    }
+
+    function turnOffLight() {
+        document.getElementById('myImage').src = 'pic_bulboff.gif';
+    }
