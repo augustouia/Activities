57d831732e41dd0ec6ef7af7dc37d1b1781f3b9c
Jhermaine Bautista <202412274@gordoncollege.edu.ph>
2025-04-07 16:59:25 +0800

    Delete Light Bulb directory

--- Light Bulb/ilawscript.js
+++
@@ -1,7 +0,0 @@
-    function turnOnLight() {
-        document.getElementById('myImage').src = 'pic_bulbon.gif';
-    }
-
-    function turnOffLight() {
-        document.getElementById('myImage').src = 'pic_bulboff.gif';
-    }
